6cc16acb85c80a6867be2789fd77806a05bc5067
Wookeun Song <wookeun.s@gmail.com>
2019-10-13 15:16:09 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Hello", last_name: "MF")
+Student.create(first_name: "YEAH", last_name: "MF")
+Student.create(first_name: "OOP", last_name: "MF")
+Student.create(first_name: "GIT", last_name: "MF")
+Student.create(first_name: "MAN", last_name: "MF")
+Student.create(first_name: "OOOOH", last_name: "MF")
+Student.create(first_name: "YAYAYAY", last_name: "MF")
+Student.create(first_name: "OHOHOO", last_name: "MF")
